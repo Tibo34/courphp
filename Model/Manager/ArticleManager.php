dd76d43e9a0bfc73e0901e9118760971067eadde
Thibaut Molina <thibautmolina@yahoo.fr>
2018-10-10 09:53:27 +0200

    first commit

---
+++ Model/Manager/ArticleManager.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Model\Manager;
+
+require_once __DIR__ . '/../../Model/Exceptions/ArticleNotFoundException.php';
+require_once __DIR__ . '/../../Model/Manager/DbManager.php';
+require_once __DIR__ . '/../../Model/Interfaces/ItemInterface.php';
+require_once __DIR__ . '/../../Model/Article/Article.php';
+
+use Model\Article\Article;
+use Model\Interfaces\ItemInterface;
+use Model\Exceptions\ArticleNotFoundException;
+
+/**
+ * Manage article from database
+ * 
+ * Class ArticleManager
+ * @package Manager
+ */
+abstract class ArticleManager extends DbManager implements ItemInterface
+{
+    /**
+     * @param int $id
+     * @return mixed|Article
+     * @throws ArticleNotFoundException
+     * @throws \Exception
+     */
+    public static function getById($id)
+    {
+        // Select article into database
+        $stmt = self::getDb()->prepare("
+          SELECT id, title, image, head, content, create_date
+          FROM articles
+          WHERE id = :id;
+        ");
+        $stmt->bindValue(":id", $id);
+        $stmt->execute();
+
+        // Throw an exception if the article hasn't been found
+        if ($stmt->rowCount() === 0) {
+            throw new ArticleNotFoundException($id);
+        }
+
+        // Get new article
+        $article = new Article();
+        $article->hydrate(
+            $stmt->fetch(\PDO::FETCH_ASSOC)
+        );
+
+        return $article;
+    }
+
+    /**
+     * @param null $offset
+     * @param null $limit
+     * @return array|mixed
+     * @throws \Exception
+     */
+    public static function getAll($offset = null, $limit = null)
+    {
+        // Select list of article in database
+        $stmt = self::getDb()->prepare("
+          SELECT id, title, image, head, content, create_date
+          FROM articles;
+        ");
+        $stmt->execute();
+
+        // Instantiates a collection of article
+        $articles = array();
+        while ($articleData = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $article = new Article();
+            $article->hydrate($articleData);
+            $articles[] = $article;
+        }
+
+        return $articles;
+    }
+}
---
+++ View/templates/home.html.php
@@ -0,0 +1,29 @@
+<!-- BEGIN : home.html.php -->
+<?php foreach ($articles as $article) : ?>
+    <div>
+        <h2><?= $article->getTitle() ?></h2>
+        <p><?= $article->getHead() ?></p>
+        <p><a class="btn btn-default" href="/index.php?page=article&id=<?= $article->getId() ?>" role="button">View details &raquo;</a></p>
+    </div>
+<?php endforeach; ?>
+
+<nav aria-label="Page navigation">
+    <ul class="pagination">
+        <li>
+            <a href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li>
+            <a href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
+    </ul>
+</nav>
+<!-- END : home.html.php -->
